1bdb8d75e503dc1795ec3ba1a435c421f05d4123
Donald Edinam <kobbyedinamdonald@gmail.com>
2023-09-22 10:10:09 +0000

    Let go get it

---
+++ 000_Weather_app/main.js
@@ -0,0 +1,19 @@
+const key = 'rhll5ZF88VHWqBx6petKbfMn1zljYL6o';
+
+
+async function getData(city){
+   
+    try {
+        const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+        const query = `?apikey=${key}&q=${city}`;
+        const apiUrl = baseUrl + query;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+    
+        console.log(data[0])
+    } catch (error) {
+        alert("Something went wrong, Check your internet connection and try again")
+    }
+}
+
+getData("Tema");
